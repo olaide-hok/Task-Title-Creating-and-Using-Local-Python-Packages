bd2ab12b7e5242e33947c752ec40e25db8e8617d
olaide-hok <olaidekareem43@yahoo.com>
2022-06-09 20:06:00 +0100

    Complete Rock Paper Scissors Challenge

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Task-Title-Creating-and-Using-Local-Python-Packages
+
+[Rock-Paper-Scissors]
+Rock-Paper-Scissors is a simple two-player game where, at a signal, players make figures with their hands, representing a rock, a piece of paper, or a pair of scissors. The winner is determined according to a set of rules. You can find the official rules under the Resources.
+
+ 
+
+A brief summary:
+
+If the two players choose the same “character” it’s a tie, and the game repeats
+Rock beats Scissors
+Paper beats Rock
+Scissors beats Paper
+You have been tasked to create a simple version of this game with Python. In your version, one player will be controlled by the computer and the other player controlled by you - the user (i.e CPU vs Player). 
+
+Inbuilt Python module random and its choice method is used.
+
+Instructions:
+
+Create a new Python file and call it main.py.
+Create a list to store all possible options:
+"R" for "rock", 
+"P" for "paper", 
+"S" for "scissors".
+When the program is run, the user is asked to pick an option between "R", "P" or "S"
+If user input is invalid (not amongst the options or an empty input), an error is printed, the user is asked for their input again (loop).
+
+Both player's moves are printed in the format: `Player (Rock) : CPU (Paper)`
+
+If there is a winner, print the winner, and the program ends. 
+If it's a tie (the computer and player pick the same move), the game is restarted.
--- main.py
+++ main.py
@@ -1,104 +1,59 @@
 # Rock-Paper-Scissors
 
 import random
-from unittest import result
 
 while True:
     options=['R', 'P', 'S']
 
     CPU = random.choice(options)
     Player = None
-
-    while Player not in options:
-        # if Player not in options:
-        #     print('Error, please type in correct a option')
-        #     break
+    Player = input('R for Rock, P for Paper, or S for scissors?: ').upper()
+    while Player not in options and Player != None:
+        print('Error, please type in correct a option')
         Player = input('R for Rock, P for Paper, or S for scissors?: ').upper()
 
-    if Player == CPU:
-        print('Player {}, CPU {} '.format(Player, CPU))
+    while (Player == CPU):
+        print('Player ({}) : CPU ({})'.format(Player, CPU))
         print('Tie!')
-    
-    elif Player == 'R':
+        Player = input('R for Rock, P for Paper, or S for scissors?: ').upper()
+ 
+    if Player == 'R':
+        Player = 'Rock'
         if CPU == 'P':
-            print(Player, CPU )
+            CPU = 'Paper'
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You lose!')
         if CPU == 'S':
-            print(Player, CPU )
+            CPU = 'Scissors'
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You win!')
     
     elif Player == 'S':
+        Player = 'Scissors'
         if CPU == 'R':
-            print(Player, CPU )
+            CPU = 'Rock'
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You lose!')
         if CPU == 'P':
-            print(Player, CPU )
+            CPU = "Paper"
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You win!')
 
     elif Player == 'P':
+        Player = 'Paper'
         if CPU == 'S':
-            print(Player, CPU )
+            CPU = 'Scissors'
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You lose!')
         if CPU == 'R':
-            print(Player, CPU )
+            CPU = 'Rock'            
+            print('Player ({}) : CPU ({})'.format(Player, CPU))
             print('You win!')
 
-    play_again = input('Play again? (yes/no): ').lower()
+    play_again = input('Play again? (yes(y)/no(n)): ').lower()
 
-    if play_again != 'yes':
+    if play_again != 'yes' or play_again != 'y':
         break
+print("Bye!")
 
 
-
-# import random
-
-# while True:
-#     choices = ["rock","paper","scissors"]
-
-#     computer = random.choice(choices)
-#     player = None
-
-#     while player not in choices:
-#         player = input("rock, paper, or scissors?: ").lower()
-
-#     if player == computer:
-#         print("computer: ",computer)
-#         print("player: ",player)
-#         print("Tie!")
-
-#     elif player == "rock":
-#         if computer == "paper":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You lose!")
-#         if computer == "scissors":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You win!")
-
-#     elif player == "scissors":
-#         if computer == "rock":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You lose!")
-#         if computer == "paper":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You win!")
-
-#     elif player == "paper":
-#         if computer == "scissors":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You lose!")
-#         if computer == "rock":
-#             print("computer: ", computer)
-#             print("player: ", player)
-#             print("You win!")
-
-#     play_again = input("Play again? (yes/no): ").lower()
-
-#     if play_again != "yes":
-#         break
-
-# print("Bye!")
